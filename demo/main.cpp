743498e1050d0d6d4abac98abdb94e53a49895ce
Phantomical <phantom@lynches.ca>
2016-11-12 17:56:57 -0500

    Made stack cause page fault in case of overflow

--- demo/main.cpp
+++ demo/main.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 #include <vector>
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+void* alloc_stack(size_t size)
+{
+	LPVOID mem = VirtualAlloc(nullptr, size + 4096, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	VirtualAlloc((char*)mem + size, 4096, MEM_DECOMMIT, PAGE_NOACCESS);
+	return mem;
+}
+
 void func(size_t start, size_t stop, size_t step)
 {
 	for (size_t i = start; i < stop; i += step)
@@ -12,22 +22,19 @@ void func(size_t start, size_t stop, size_t step)
 template<typename T>
 gen_wrapper<T> range(T start, T stop, T step = 1)
 {
+	static constexpr size_t stack_size = 4096;
 	return make_generator<T>([=]
 	{
 		for (size_t i = start; i < stop; i += step)
 			yield(i);
-	}, 1024 * 1024);
+	}, stack_size, alloc_stack(stack_size));
 }
 
 int main(int argc, char** argv)
 {
-	auto gen = range<size_t>(0, 100, 1).gen;
-	if (!gen->complete())
+	for (auto val : range<size_t>(0, 100))
 	{
-		do
-		{
-			std::cout << (gen->value()) << '\n';
-		} while (gen->next());
+		std::cout << val << '\n';
 	}
 
 	std::cout << std::endl;
