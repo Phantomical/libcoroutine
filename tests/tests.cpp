6c7d6ea2bab58d70bdbf7a8cd431aa41a8d4e52d
Phantomical <phantom@lynches.ca>
2016-12-14 23:17:47 -0500

    Added unit test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,25 @@
+#include "coroutine.h"
+#include "gtest\gtest.h"
 
+void deterministic_test(void* arg)
+{
+	context* ctx = static_cast<context*>(arg);
+	for (uintptr_t i = 0; i < 64; ++i)
+	{
+		coroutine_yield(ctx, reinterpret_cast<void*>(i));
+	}
+}
+
+TEST(run, yield_unmodified)
+{
+	context* ctx = coroutine_start({ 16382, &deterministic_test });
+
+	EXPECT_EQ(0, reinterpret_cast<uintptr_t>(coroutine_next(ctx, ctx)));
+
+	for (uintptr_t i = 1; i < 64; ++i)
+	{
+		EXPECT_EQ(i, reinterpret_cast<uintptr_t>(
+			coroutine_next(ctx, reinterpret_cast<void*>(i))
+		));
+	}
+}
