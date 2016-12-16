a8c3029d9e8911b5634206485b4bba5c8e0ba9a2
Phantomical <phantom@lynches.ca>
2016-12-15 19:17:33 -0500

    Added test for coroutine_start

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,8 @@
 
 #include <utility>
 
+#define TEST_COROUTINE_STACK_SIZE 16384
+
 void deterministic_test(void* arg)
 {
 	context* ctx = static_cast<context*>(arg);
@@ -20,10 +22,15 @@ void set_var(void* arg)
 
 	*vals->second = 0xFFF;
 }
+void set_var_before(void* arg)
+{
+	std::pair<context*, int*>* vals = (std::pair<context*, int*>*)arg;
+	*vals->second = 0xFFF;
+}
 
 TEST(run, yield_unmodified)
 {
-	context* ctx = coroutine_start({ 16382, &deterministic_test });
+	context* ctx = coroutine_start({ TEST_COROUTINE_STACK_SIZE, &deterministic_test });
 
 	EXPECT_EQ(0, reinterpret_cast<uintptr_t>(coroutine_next(ctx, ctx)));
 
@@ -36,12 +43,11 @@ TEST(run, yield_unmodified)
 
 	coroutine_abort(ctx);
 }
-
 TEST(run, destroy_completes_coroutine)
 {
 	int r = 0;
 	std::pair<context*, int*> pair;
-	pair.first = coroutine_start({ 16382, &set_var });
+	pair.first = coroutine_start({ TEST_COROUTINE_STACK_SIZE, &set_var });
 	pair.second = &r;
 
 	coroutine_next(pair.first, &pair);
@@ -50,12 +56,11 @@ TEST(run, destroy_completes_coroutine)
 
 	ASSERT_EQ(r, 0xFFF);
 }
-
 TEST(run, abort_does_not_complete)
 {
 	int r = 0;
 	std::pair<context*, int*> pair;
-	pair.first = coroutine_start({ 16382, &set_var });
+	pair.first = coroutine_start({ TEST_COROUTINE_STACK_SIZE, &set_var });
 	pair.second = &r;
 
 	coroutine_next(pair.first, &pair);
@@ -64,10 +69,9 @@ TEST(run, abort_does_not_complete)
 
 	ASSERT_NE(r, 0xFFF);
 }
-
-TEST(run, is_complete_works)
+TEST(run, is_complete)
 {
-	context* ctx = coroutine_start({ 16382, &deterministic_test });
+	context* ctx = coroutine_start({ TEST_COROUTINE_STACK_SIZE, &deterministic_test });
 
 	EXPECT_EQ(0, reinterpret_cast<uintptr_t>(coroutine_next(ctx, ctx)));
 
@@ -76,10 +80,25 @@ TEST(run, is_complete_works)
 		EXPECT_EQ(i, reinterpret_cast<uintptr_t>(
 			coroutine_next(ctx, reinterpret_cast<void*>(i))
 			));
-		EXPECT_EQ((bool)coroutine_is_complete(ctx), !(i < 64));
+		EXPECT_TRUE(coroutine_is_complete(ctx) != 0);
 	}
 	(void)coroutine_next(ctx, nullptr);
 
-	EXPECT_EQ((bool)coroutine_is_complete(ctx), true);
+	EXPECT_TRUE(coroutine_is_complete(ctx) != 0);
 	coroutine_destroy(ctx, nullptr);
+}
+TEST(run, start_does_not_call_function)
+{
+	int i = 0;
+	context* ctx = coroutine_start({ TEST_COROUTINE_STACK_SIZE, &set_var_before });
+	// Make sure that null is not returned
+	ASSERT_NE(ctx, nullptr);
+	std::pair<context*, int*> vals = { ctx, &i };
+	// Make sure i has not changed
+	ASSERT_EQ(i, 0);
+	coroutine_next(ctx, &vals);
+	// Make sure i was set
+	ASSERT_EQ(i, 0xFFF);
+
+	coroutine_abort(ctx);
 }
